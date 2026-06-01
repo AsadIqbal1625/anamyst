4cd24e2f28c14aded81b95d04d06236564fd13e8
AsadIqbal1625 <asadiqbalamu@gmail.com>
2026-06-01 16:47:37 +0400

    track

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,103 +1,63 @@
-import { connectDB }
-from "../../../lib/mongodb";
-import {
-  sendOrderEmail,
-} from "../../../lib/sendOrderEmail";
-import Order
-from "../../../models/Order";
+import { connectDB } from "../../../lib/mongodb";
+import { sendOrderEmail } from "../../../lib/sendOrderEmail";
+import Order from "../../../models/Order";
 
 /* GET ORDERS */
 export async function GET() {
-
   try {
-
     await connectDB();
 
-    const orders =
-      await Order.find()
-        .sort({
-          createdAt: -1,
-        });
-        await sendOrderEmail(order);
+    const orders = await Order.find().sort({
+      createdAt: -1,
+    });
 
     return Response.json({
-
       success: true,
-
       orders,
-
     });
-    
-
   } catch (error) {
-
     console.log(error);
 
     return Response.json({
-
       success: false,
-
       error: error.message,
-
     });
-
   }
-
 }
 
 /* CREATE ORDER */
 export async function POST(req) {
-
   try {
-
     await connectDB();
 
-    const body =
-      await req.json();
+    const body = await req.json();
 
     /* ORDER ID */
     const orderId =
-
       "ANM-" +
-
       Math.floor(
-        100000 +
-        Math.random() *
-        900000
+        100000 + Math.random() * 900000
       );
 
-    /* CREATE */
-    const order =
-      await Order.create({
+    /* CREATE ORDER */
+    const order = await Order.create({
+      ...body,
+      orderId,
+    });
 
-        ...body,
-
-        orderId,
-
-      });
-
+    /* SEND EMAIL */
     await sendOrderEmail(order);
 
-        return Response.json({
-
-          success: true,
-
-          order,
-
-        });
-
+    return Response.json({
+      success: true,
+      order,
+    });
   } catch (error) {
-
     console.log(error);
 
     return Response.json({
-
       success: false,
-
       error: error.message,
-
     });
-
   }
-
 }
